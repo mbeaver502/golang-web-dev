4849c1972e330ef3756db8d5f767a7cbed8ae607
J. Michael Beaver <jmbeaver1@gmail.com>
2022-07-18 18:48:20 -0500

    Remove Redundant New Line

--- 015_understanding-TCP-servers/07_tcp-apps/02_memory-database/main.go
+++ 015_understanding-TCP-servers/07_tcp-apps/02_memory-database/main.go
@@ -56,7 +56,7 @@ func handle(conn net.Conn) {
 			fmt.Fprintf(conn, "%s\r\n", v)
 		case "SET":
 			if len(fs) != 3 {
-				fmt.Fprintln(conn, "EXPECTED VALUE\r\n")
+				fmt.Fprintln(conn, "EXPECTED VALUE")
 				continue
 			}
 			k := fs[1]
